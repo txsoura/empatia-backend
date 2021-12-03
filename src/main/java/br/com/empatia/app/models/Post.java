1fe569da0cd6765716a4d7adf9da772441e5f3d5
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-12-02 22:09:21 -0300

    feat(model): create post

---
+++ src/main/java/br/com/empatia/app/models/Post.java
@@ -0,0 +1,141 @@
+package br.com.empatia.app.models;
+
+import br.com.empatia.app.enums.PostType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@Table(name = "posts")
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private PostType type;
+
+    @Column(nullable = false)
+    private String content;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private User user;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Post post;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Challenge challenge;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<User> views = new ArrayList<>();
+
+    @Column
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    public Post(String content) {
+        this.content = content;
+    }
+
+    public Post(PostType type, String content, User user, Post post, Challenge challenge) {
+        this.type = type;
+        this.content = content;
+        this.post = post;
+        this.challenge = challenge;
+        this.user = user;
+    }
+
+    public Post(PostType type, String content, User user) {
+        this.type = type;
+        this.content = content;
+        this.user = user;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public PostType getType() {
+        return type;
+    }
+
+    public void setType(PostType type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public Challenge getChallenge() {
+        return challenge;
+    }
+
+    public void setChallenge(Challenge challenge) {
+        this.challenge = challenge;
+    }
+
+    public List<User> getViews() {
+        return views;
+    }
+
+    public void setViews(List<User> views) {
+        this.views = views;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
