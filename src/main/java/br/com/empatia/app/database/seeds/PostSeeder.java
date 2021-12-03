525b8b9cb8b1ea0243f9ce2e4159d7f3add1c2ed
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-12-02 22:20:09 -0300

    feat(seeds): create post

---
+++ src/main/java/br/com/empatia/app/database/seeds/PostSeeder.java
@@ -0,0 +1,44 @@
+package br.com.empatia.app.database.seeds;
+
+import br.com.empatia.app.enums.PostType;
+import br.com.empatia.app.models.Post;
+import br.com.empatia.app.models.User;
+import br.com.empatia.app.repositories.PostRepository;
+import com.github.javafaker.Faker;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Component
+@AllArgsConstructor
+public class PostSeeder {
+    public static Faker faker = new Faker();
+    PostRepository repository;
+
+    public static Post create() {
+        int type = new Random().nextInt(PostType.values().length);
+
+        User user = UserSeeder.create();
+
+        return new Post(PostType.values()[type], faker.lorem().sentences(2).toString(), user);
+    }
+
+    public void run(int x) {
+        repository.saveAll(
+                seeds(x)
+        );
+    }
+
+    private List<Post> seeds(int x) {
+        List<Post> posts = new ArrayList<>();
+
+        for (int i = 0; i < x; i++) {
+            posts.add(create());
+        }
+
+        return posts;
+    }
+}
